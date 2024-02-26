2157c239d41311589a45ab9bbb3ccc5b3b3b7d12
Tim <50870607+toothless13@users.noreply.github.com>
2024-02-26 15:27:54 +0000

    animate ListItem component to appear from below

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 const ListItem = ({ todo, completed, id, handleComplete, handleUpdate, handleDelete }) => {
 
   const [editing, setEditing] = useState(false);
   const [editText, setEditText] = useState(todo);
+  const springs = useSpring({
+    from: { 
+            y: 50,
+            opacity: 0,
+           },
+    to: { 
+          y: 0,
+          opacity: 1, 
+        },
+  });
 
   const handleFieldChange = e => {
     setEditText(e.target.value);
@@ -20,7 +31,7 @@ const ListItem = ({ todo, completed, id, handleComplete, handleUpdate, handleDel
   const todoTextClass = "col-span-5 text-left truncate";
 
   return (
-    <div className="grid grid-cols-7 ga-3 items-center border-b-2 border-zinc-300 my-5 p-2">
+    <animated.div className="grid grid-cols-7 ga-3 items-center border-b-2 border-zinc-300 my-5 p-2" style={{...springs}}>
       <input type="checkbox" onClick={() => handleComplete(id)} className="ml-1 mr-3 col-span-1 appearance-none w-4 h-4 border-2 rounded-full text-zinc-300 indeterminate:bg-zinc-300 checked:to-zinc-300 checked:bg-teal-400 hover:cursor-pointer" />
       <div className={completed ?  todoTextClass + " done" : todoTextClass}
       onClick={() => {
@@ -53,7 +64,7 @@ const ListItem = ({ todo, completed, id, handleComplete, handleUpdate, handleDel
       )}
       </div>
       <button onClick={() => handleDelete(id)} className="text-xl text-red-600 col-span-1" >&times;</button> 
-  </div>
+  </animated.div>
   )
 }
 
