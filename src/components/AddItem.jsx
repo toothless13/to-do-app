3a84e8f41251dc233f060d14149cdac8c205bcf8
Tim <50870607+toothless13@users.noreply.github.com>
2024-02-28 16:40:59 +0000

    start fetching data from firestore to fill array

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,15 +1,47 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
+import { Context } from "../Context/AuthContext"
+import { setDoc, doc, serverTimestamp } from "firebase/firestore"
+import { db } from "../config/firebase"
 import { v4 as uuidv4 } from 'uuid';
 
 const AddItem = ({ list, setList }) => {
 
+  const { user } = useContext(Context);
   const inputRef = useRef();
 
-  const handleAddItem = e => {
+  // const handleAddItem = e => {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.target);
+  //   const userInput = formData.get("to-do-item");
+  //   if(userInput !== "") {
+  //     setList([...list, {todo: userInput, completed: false, id: uuidv4()}]);
+  //     inputRef.current.value = "";
+  //   }
+  // }
+  console.log(user);
+
+  const handleAddItem = async e => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const userInput = formData.get("to-do-item");
     if(userInput !== "") {
+      const task = {
+        todo: userInput, 
+        completed: false, 
+        id: uuidv4(),
+      }
+      if (user) {
+        try {
+          const test = await setDoc(doc(db, "tasks", `${task.id} - ${user.uid}`), {
+            uid: user.uid,
+            task: { ...task },
+            createdAt: serverTimestamp()
+          });
+          console.log(test);
+        } catch (error) {
+          console.log(error);
+        }
+      }
       setList([...list, {todo: userInput, completed: false, id: uuidv4()}]);
       inputRef.current.value = "";
     }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,25 +12,45 @@ const HomePage = () => {
 
   const initialState = {
     list: [
-      {
-        todo: "test 1",
-        completed: false,
-        id: uuidv4()
-      },
-      {
-        todo: "test 2",
-        completed: false,
-        id: uuidv4()
-      },
-      {
-        todo: "test 3",
-        completed: false,
-        id: uuidv4()
-      }
+      // {
+      //   todo: "test 1",
+      //   completed: false,
+      //   id: uuidv4()
+      // },
+      // {
+      //   todo: "test 2",
+      //   completed: false,
+      //   id: uuidv4()
+      // },
+      // {
+      //   todo: "test 3",
+      //   completed: false,
+      //   id: uuidv4()
+      // }
     ],
   }
 
-  const [list, setList] = useState(initialState.list);
+
+
+  const [list, setList] = useState([]);
+
+  const getUserTasks = useCallback(async () => {
+    if (user) {
+      const querySnapshot = await getDocs(collection(db, "tasks"));
+      querySnapshot.forEach((doc) => {
+        if (doc.data().uid === user.uid) {
+          setList((prevState) => {
+            return [...prevState, doc.data().task];
+          });
+        }
+      });      
+    }
+
+  },[setList, user])
+
+  useEffect(() => {
+    getUserTasks();
+  }, []);
 
   return (
     <div className="w-4/5 md:w-3/4 lg:w-1/3 mx-auto text-center rounded-lg border-2 border-zinc-300 my-5 p-2">
