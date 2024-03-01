b479d5203cd17a9cf1537a048ce48df8f71a0838
Tim <50870607+toothless13@users.noreply.github.com>
2024-03-01 12:48:18 +0000

    add page reload when logout to remove logged in user list

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,6 +19,7 @@ const NavBar = () => {
         console.log("User logged out");
         // setLoggedOut(true);
         navigate("/");
+        navigate(0);
       })
       .catch((error) => {
         console.log(error);
