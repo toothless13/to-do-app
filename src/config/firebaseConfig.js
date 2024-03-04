cc1a368cb97acb6879f0b031e914826008a18408
Tim <50870607+toothless13@users.noreply.github.com>
2024-03-04 12:34:43 +0000

    include firebaseConfig

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkrX9xJ2bhpYOnhcsmqPph21LX4W3TfWM",
+  authDomain: "to-do-app-ebfb8.firebaseapp.com",
+  projectId: "to-do-app-ebfb8",
+  storageBucket: "to-do-app-ebfb8.appspot.com",
+  messagingSenderId: "192820364375",
+  appId: "1:192820364375:web:c7e66142e207e012b24f12",
+  measurementId: "G-P1EZPV67NX"
+};
+
+export default firebaseConfig;
